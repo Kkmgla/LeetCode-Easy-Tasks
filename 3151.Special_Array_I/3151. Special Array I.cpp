163fa26f0bbb805152830b4479d2056bc92ea58c
Kk <143418966+Kkmgla@users.noreply.github.com>
2025-02-01 21:17:29 +0300

    Add files via upload

---
+++ 3151.Special_Array_I/3151. Special Array I.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        if (nums.size() == 0) return 1;
+        else if (nums.size() == 1) return 1;
+        int i = 0;
+        while (i + 1 < nums.size())
+        {
+            if (!(((nums[i] % 2 == 0) && (nums[i + 1] % 2 != 0)) || ((nums[i + 1] % 2 == 0) && (nums[i] % 2 != 0))))
+            {
+                return 0;
+            }
+            i++;
+        }
+        return 1;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> nums = {1,2,3,4,5,6,7,8,9};
+    cout << s.isArraySpecial(nums);
+}
