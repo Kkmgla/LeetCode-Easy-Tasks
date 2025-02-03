dd846292dec0dde1187606380e3b848f3db7b661
Kk <143418966+Kkmgla@users.noreply.github.com>
2025-02-03 17:36:50 +0300

    Add files via upload

---
+++ 3105.Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int maxMonoticSubarraylength = 1;
+        int currMonoticSubarraylength = 0;
+        int direction = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                if (direction != 3) currMonoticSubarraylength++;
+                if (direction == 1)
+                {
+                    if (currMonoticSubarraylength > maxMonoticSubarraylength) {
+                        maxMonoticSubarraylength = currMonoticSubarraylength;
+                    }
+                    currMonoticSubarraylength = 1;
+                }
+                direction = 0;
+            }
+            else if (nums[i] > nums[i + 1])
+            {
+                if (direction != 3) currMonoticSubarraylength++;
+                if (direction == 0) 
+                {
+                    if (currMonoticSubarraylength > maxMonoticSubarraylength) {
+                        maxMonoticSubarraylength = currMonoticSubarraylength;
+                    }
+                    currMonoticSubarraylength = 1;
+                }
+                direction = 1;
+            }
+            else
+            {
+                if (direction != 3) currMonoticSubarraylength++;
+                if (currMonoticSubarraylength > maxMonoticSubarraylength) {
+                    maxMonoticSubarraylength = currMonoticSubarraylength;
+                }
+                currMonoticSubarraylength = 1;
+                direction = 3;
+            }
+        }
+
+        if (direction != 3) currMonoticSubarraylength++;
+        if (currMonoticSubarraylength > maxMonoticSubarraylength) return currMonoticSubarraylength;
+        return maxMonoticSubarraylength;
+    }
+};
+
+
+int main()
+{
+    Solution s;
+    vector<int> nums = {3,3,3,3};
+    cout << s.longestMonotonicSubarray(nums);
+}
