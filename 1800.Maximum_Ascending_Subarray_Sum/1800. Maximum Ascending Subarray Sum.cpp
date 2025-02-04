5100311de0fee059da87c6c0633ef8b95aa8b2d3
Kk <143418966+Kkmgla@users.noreply.github.com>
2025-02-04 17:37:35 +0300

    Add files via upload

---
+++ 1800.Maximum_Ascending_Subarray_Sum/1800. Maximum Ascending Subarray Sum.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int maxAscendingSubarraySum = nums[0];
+        int currAscendingSubarraySum = nums[0];
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] < nums[i + 1]) currAscendingSubarraySum += nums[i + 1];
+            else
+            {
+                if (maxAscendingSubarraySum < currAscendingSubarraySum) maxAscendingSubarraySum = currAscendingSubarraySum;
+                currAscendingSubarraySum = nums[i + 1];
+            }
+        }
+        if (currAscendingSubarraySum > maxAscendingSubarraySum) return currAscendingSubarraySum;
+        return maxAscendingSubarraySum;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums{ 10,20,30,30,30,10,70,50 };
+    s.maxAscendingSum(nums);
+}
