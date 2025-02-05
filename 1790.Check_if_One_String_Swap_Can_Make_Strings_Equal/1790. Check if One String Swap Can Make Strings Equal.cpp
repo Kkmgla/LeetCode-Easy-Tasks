08dbed2bfd33a7317388bdde591480650fd040df
Kk <143418966+Kkmgla@users.noreply.github.com>
2025-02-05 16:54:13 +0300

    Add files via upload

---
+++ 1790.Check_if_One_String_Swap_Can_Make_Strings_Equal/1790. Check if One String Swap Can Make Strings Equal.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        if (s1 == s2) return 1;
+        int counter = 0;
+        vector <int>positions;
+        for (int i = 0; i < s1.length(); i++)
+        {
+            if (s1[i] != s2[i]) {
+                counter++;
+                positions.push_back(i);
+            }
+            if (counter > 2) return 0;
+        }
+        if (counter != 2) return 0;
+        char ch = s2[positions[1]];
+        s2[positions[1]] = s2[positions[0]];
+        s2[positions[0]] = ch;
+        if (s1 == s2) return 1;
+
+        return 0;
+    }
+};
+
+int main()
+{
+    Solution s;
+    string s1 = "bank";
+    string s2 = "kanb";
+    cout << s.areAlmostEqual(s1, s2);
+}
