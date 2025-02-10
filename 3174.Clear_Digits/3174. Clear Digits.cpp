270e065fd08c71dca9595b823d23e67ef1296b4e
Kk <143418966+Kkmgla@users.noreply.github.com>
2025-02-10 17:57:59 +0300

    Add files via upload

---
+++ 3174.Clear_Digits/3174. Clear Digits.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    string clearDigits(string s) {
+        string stack;
+        for (char c : s) {
+            if (isdigit(c)) {
+                if (!stack.empty()) stack.pop_back();
+            }
+            else {
+                stack.push_back(c);
+            }
+        }
+        return stack;
+    }
+};
+
+
+int main()
+{
+    Solution s;
+    string str = "cd34";
+    cout << s.clearDigits(str);
+}
