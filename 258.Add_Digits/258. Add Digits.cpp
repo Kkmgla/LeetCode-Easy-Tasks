0373b725a54c5444fe1fc3fa19dcb55517035e64
Kk <143418966+Kkmgla@users.noreply.github.com>
2025-02-10 19:00:18 +0300

    Add files via upload

---
+++ 258.Add_Digits/258. Add Digits.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	int addDigits(int num) {
+		if (num >= 0 && num <= 9) return num;
+		int tmp = 0;
+		while (num != 0)
+		{
+			tmp += num % 10;
+			num = num / 10;
+		}
+		return addDigits(tmp);
+    }
+};
+
+int main()
+{
+	Solution s;
+	int num = 38;
+	cout << s.addDigits(num);
+}
